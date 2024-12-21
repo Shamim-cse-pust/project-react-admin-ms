2d4c28ee98ecad46d944ad873c2cb28c1b78632b
Md. Shamim Miah <shamim.miah@brainstation-23.com>
2024-12-22 01:26:29 +0600

    add Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
-import Dashboard from "./components/Dashboard";
-import Menu from "./components/Menu";
-import Nav from "./components/Nav";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./secure/Dashboard";
+import Users from "./secure/Users";
+import Menu from "./secure/components/Menu";
+import Nav from "./secure/components/Nav";
+
 function App() {
   return (
-    <>
-      <Nav/>
-      <div className="container-fluid">
-        <div className="row">
-          <Menu/>
-          <main role="main" className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-            <Dashboard/>
-          </main>
+    <BrowserRouter>
+      <div className="App">
+        <Nav />
+        <div className="container-fluid">
+          <div className="row">
+            <Menu />
+            <main role="main" className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/users" element={<Users />} />
+              </Routes>
+            </main>
+          </div>
         </div>
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
---
+++ src/secure/Users.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Users = () => (
+    <h1>Hello Users</h1>
+)
+
+export default Users;
