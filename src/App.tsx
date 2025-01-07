be4b780b8708d23bf5304cba7a9c2091477450f3
Md. Shamim Miah <shamim.miah@brainstation-23.com>
2025-01-07 19:29:59 +0600

    add Wrapper and props.children

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,17 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./secure/Dashboard";
 import Users from "./secure/Users";
-import Menu from "./secure/components/Menu";
-import Nav from "./secure/components/Nav";
+import Login from "./public/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Nav />
-        <div className="container-fluid">
-          <div className="row">
-            <Menu />
-            <main role="main" className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-              <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/users" element={<Users />} />
-              </Routes>
-            </main>
-          </div>
-        </div>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
--- src/secure/Users.tsx
+++ src/secure/Users.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import Wrapper from "./Wrapper";
 
 const Users = () => (
-    <h1>Hello Users</h1>
+    <Wrapper>
+        <h1>Hello Users</h1>
+    </Wrapper>
 )
 
 export default Users;
